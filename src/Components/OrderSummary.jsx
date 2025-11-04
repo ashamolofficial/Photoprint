2bc0db45ab194fe1561f60876c17797b4b571569
asha <ashamold2002@gmail.com>
2025-11-04 14:44:31 +0400

    third commit

--- src/Components/OrderSummary.jsx
+++ src/Components/OrderSummary.jsx
@@ -25,7 +25,7 @@ export default function OrderSummary({ photo }) {
       <Confetti />
       <h2 className="text-2xl font-bold mb-4">Order Summary</h2>
       <motion.img
-        src={`https://photoprintbackend.onrender.com/uploads/${photo.filename}`}
+        src={`https://photoprintbackend.onrender.com/api/uploads/${photo.filename}`}
         alt="selected"
         className="w-64 h-64 rounded-xl object-cover"
         whileHover={{ scale: 1.05 }}
